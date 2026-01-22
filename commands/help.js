dc3a648d4382abc02a28ae61315ffeba4023c5d8
Kamal Pokhriya <kamalchandrapokhriya43@gmail.com>
2026-01-22 10:24:34 +0530

    Simplify help message by removing forwarding info
    Removed unnecessary forwarding information from the help message.

--- commands/help.js
+++ commands/help.js
@@ -234,11 +234,7 @@ Join our channel for updates:`;
                 image: imageBuffer,
                 caption: helpMessage,
                 contextInfo: {
-                    forwardingScore: 1,
-                    isForwarded: true,
-                    forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+        
                         serverMessageId: -1
                     }
                 }
