7d868cef9b86e7601917e3e20223fb43e692ade4
Kamal Pokhriya <kamalchandrapokhriya43@gmail.com>
2026-01-22 10:22:36 +0530

    Clean up global variables in main.js
    Removed unused global variables and channelInfo object.

--- main.js
+++ main.js
@@ -146,21 +146,8 @@ const soraCommand = require('./commands/sora');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
 
 // Add this near the top of main.js with other global configurations
-const channelInfo = {
-    contextInfo: {
-        forwardingScore: 1,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
-            serverMessageId: -1
-        }
-    }
-};
 
 async function handleMessages(sock, messageUpdate, printLog) {
     try {
